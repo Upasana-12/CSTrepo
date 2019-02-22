90494eee04ae1cf2a8b3f59c150059b658248d17
Upasana-12 <upasana12000@gmail.com>
2019-02-22 10:06:39 +0530

    testing commit

---
+++ linked list.cpp
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+	int data;
+	struct node* next;
+};
+
+struct node* create(int n)
+{
+    struct node* head=0;
+	struct node* temp=0;
+	struct node* p=0;
+	int i,x;
+	for(i=0;i<n;i++)
+	{
+	struct node* new_node=(struct node*)malloc(sizeof(struct node));
+	scanf("%d",&x);
+	new_node->data=x;
+	new_node->next=0;
+	if(head==0)
+	{
+	head=new_node;
+	
+    }
+	else
+	{
+	      p=head;
+	      while(p->next!=0)
+	      p=p->next;
+	      p->next=new_node;
+	}
+	}
+	return head;
+}
+int main()
+{
+	int i,num;
+	struct node* h;
+	scanf("%d",&num);
+	h=create(num);
+
+	struct node* n;
+	for(n=h;n!=0;n=n->next)
+	{
+		printf("%d ",n->data);
+	}
+	return 0;
+}
+
